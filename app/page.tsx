89a8cc2f209f586f4af88399eef0a970800eaab1
Sway Lu <swlu@shoalter.com>
2025-02-26 12:28:15 +0800

    fix: scroll bottom layer to trigger top layer expanding

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,18 @@
 import { useState } from 'react'
 import BottomLayer from './components/bottom-layer'
 import TopLayer from './components/top-layer'
+import { useMotionValueEvent, useScroll } from 'motion/react'
 
 export default function Home() {
   const [isTopExpanded, setTopExpanded] = useState(false)
+
+  const { scrollYProgress } = useScroll()
+
+  useMotionValueEvent(scrollYProgress, 'change', (y) => {
+    if (y >= 1) {
+      setTopExpanded(true)
+    }
+  })
 
   return (
     <div className="overflow-x-hidden">
